a250a58affe95e234cac8b597386887f8daca8c4
Brandon Borders <brandon@camelcased.com>
2015-09-05 17:08:13 -0700

    Disable queue view when it is empty

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -29,19 +29,23 @@ var AppView = Backbone.View.extend({
     this.$el.attr('id', 'player');
 
     this.songQueueView.$el.on('mouseenter', (function() {
-      this.playerView.$el.velocity({bottom: ['60px', '0px']}, {duration: 500, queue: false});
-      this.songQueueView.$el.velocity({bottom: ['-338px', '-388px']}, {duration: 500, queue: false});
-      this.songQueueView.$el.css('cursor', 'pointer');
-      this.songQueueView.$el.on('click', (function(){
-        this.playerView.$el.velocity({bottom: ['398px', '60px']}, {duration: 1000, queue: false});
-        this.songQueueView.$el.velocity({bottom: ['0px', '-338px']}, {duration: 1000, queue: false});
-        this.songQueueView.$el.css('cursor', 'default');
-      }).bind(this));
+      if (this.model.get('songQueue').length > 0) {
+        this.playerView.$el.velocity({bottom: ['60px', '0px']}, {duration: 500, queue: false});
+        this.songQueueView.$el.velocity({bottom: ['-338px', '-388px']}, {duration: 500, queue: false});
+        this.songQueueView.$el.css('cursor', 'pointer');
+        this.songQueueView.$el.on('click', (function(){
+          this.playerView.$el.velocity({bottom: ['398px', '60px']}, {duration: 1000, queue: false});
+          this.songQueueView.$el.velocity({bottom: ['0px', '-338px']}, {duration: 1000, queue: false});
+          this.songQueueView.$el.css('cursor', 'default');
+        }).bind(this));
+      }
     }).bind(this));
 
     this.songQueueView.$el.on('mouseleave', (function() {
-      this.playerView.$el.velocity({bottom: ['0px', this.playerView.$el.css('bottom')]}, {duration: 500, queue: false});
-      this.songQueueView.$el.velocity({bottom: ['-388px', this.songQueueView.$el.css('bottom')]}, {duration: 500, queue: false});
+      if (this.model.get('songQueue').length > 0) {
+        this.playerView.$el.velocity({bottom: ['0px', this.playerView.$el.css('bottom')]}, {duration: 500, queue: false});
+        this.songQueueView.$el.velocity({bottom: ['-388px', this.songQueueView.$el.css('bottom')]}, {duration: 500, queue: false});
+      }
     }).bind(this));
 
     return this.$el.html([
